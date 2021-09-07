c42eca7abfea5f0df69e837ecc05e01826ff07d3
DanWuPet <danx1698@edu.easj.dk>
2021-09-07 12:38:23 +0200

    Upload error

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Exercise 1
+
+        int age = 19;
+        double kmhjem = 28.2;
+
+        //The system won't allow it since the variables doesn't fill the requirements.
+        System.out.printf("My own age = %.3f", age);
+        System.out.printf("How far from home I am: %,d", kmhjem);
+
+    }
+}
